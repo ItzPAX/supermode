c82b0201f8f27ded190cb3fd60cf330cec5631a3
ItzPAX <54080059+ItzPAX@users.noreply.github.com>
2023-11-05 14:21:41 +0100

    Update settings.h

--- settings.h
+++ settings.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <string>
 
+/// only use if EAC encrypts the DTB in EPROCESS
+/// requires you to start supermode.exe while the game is running
 static constexpr bool EACCR3 = false;
 
 static std::string LOCAL_NAME = "supermode_me.exe";
